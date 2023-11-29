95a463bd8cd1092d326a385c56819d8a5c548a43
Gabriel-Galdino22 <rm550711@fiap.com>
2023-11-29 11:31:15 -0300

    Criação da Home.jsx e App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Home from "./routes/Home";
+
+export default function App(){
+return(
+  <div>
+    <h1>Olá Mundo!</h1>
+    <Home/>
+  </div>
+)
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+//ROTAS
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+//ROTAS
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
